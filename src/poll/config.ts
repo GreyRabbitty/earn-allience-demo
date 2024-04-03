e049c8ed52e7b910a942abec7e4cb7b57cb04953
miloje nikolic <nikolic.miloje0507@gmail.com>
2024-04-03 11:21:17 +0200

    Co-authored-by: GreyRabbitty <greyRabbitty@gmail.com> Co-authored-by: MilojeBtc <nikolic.miloje0507@gmail.com>

--- src/poll/config.ts
+++ src/poll/config.ts
@@ -1,5 +1,5 @@
 export const DB_URL = "http://earn-alliance/postgres/db";
 export const DB_password = "earnAlliance";
 export const DB_PORT = 2000;
-export const DB_NAME = "EARN-Allaince";
+export const DB_NAME = "Earn-Allaince";
 
