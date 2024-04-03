1bcc96ef0427b61ffa8b67eeb06971620b158b49
milojeBtc <nikolic.miloje0507@gmail.com>
2024-04-03 09:40:06 +0200

    Update config.ts

--- src/poll/config.ts
+++ src/poll/config.ts
@@ -2,4 +2,4 @@ export const DB_URL = "http://earn-alliance/postgres/db";
 export const DB_password = "earnAlliance";
 export const DB_PORT = 2000;
 export const DB_NAME = "EARN-Allaince";
-
+export const DB_TABLe_NAME = "Earn-Alliance"
