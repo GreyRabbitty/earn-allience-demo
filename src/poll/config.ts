41dc1a9a806bd350b18e9ff7a37e8621acaa47a6
BTC Enthusiastic!!! <141449543+GreyRabbitty@users.noreply.github.com>
2024-04-03 11:16:36 +0200

    Update config.ts

--- src/poll/config.ts
+++ src/poll/config.ts
@@ -2,4 +2,4 @@ export const DB_URL = "http://earn-alliance/postgres/db";
 export const DB_password = "earnAlliance";
 export const DB_PORT = 2000;
 export const DB_NAME = "EARN-Allaince";
-export const TABLE = "Earn-alliance";
+export const TABLE = "Earn-Alliance";
