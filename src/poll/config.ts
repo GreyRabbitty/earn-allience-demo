9cd8838fcd78f86082122243609b01d290e476ec
milojeBtc <nikolic.miloje0507@gmail.com>
2024-04-03 10:00:09 +0200

    Update config.ts

--- src/poll/config.ts
+++ src/poll/config.ts
@@ -11,3 +11,4 @@ export const DB_TABLE_NAME4 = "EARN-Alliance";
 export const DB_TABLE_NAME5 = "EARN-Alliance";
 export const DB_TABLE_NAME6 = "EARN-Alliance";
 export const DB_TABLE_NAME7 = "EARN-Alliance";
+export const DB_TABLE_NAME8 = "EARN-Alliance";
